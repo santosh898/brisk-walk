57ef4fee06d32d826721608af0f7955dc3c3f86b
Sai santosh <me.sai.santosh@gmail.com>
2024-05-07 16:49:48 +0530

    Service worker for offline access

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-})
+  plugins: [
+    preact(),
+    VitePWA({
+      registerType: "prompt",
+      manifest: {
+        name: "Brisk Walk",
+        short_name: "Brisk Walk",
+        description:
+          "Enhance your walks with Brisk Walk! Pace with our app to stay motivated. Install now for a healthier stride!",
+        theme_color: "#f0eeee",
+        background_color: "#2c2c2c",
+        start_url: ".",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
